abb4a7f29e1e51eb0ba40c132830e6f9208049d6
ddenko <damian.dejnek@gmail.com>
2015-06-10 07:11:01 +0200

    WCF initialization

--- MockCompany_develop/ConsoleHost/Program.cs
+++ MockCompany_develop/ConsoleHost/Program.cs
@@ -1,8 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceModel;
+using Autofac;
+using Autofac.Integration.Wcf;
+using Business.Managers;
+using Data.Repositories;
 
 namespace ConsoleHost
 {
@@ -10,6 +12,28 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
+            IContainer container = null;
+            ContainerBuilder containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterAssemblyTypes(typeof(SalesRepository).Assembly)
+                .Where(t => t.Name.EndsWith("Repository"))
+                .As(t => t.GetInterfaces().FirstOrDefault(
+                    i => i.Name == "I" + t.Name));
+
+            containerBuilder.RegisterType<SalesManager>();
+
+            container = containerBuilder.Build();
+
+            ServiceHost hostSalesManager = new ServiceHost(typeof(SalesManager));
+
+            hostSalesManager.AddDependencyInjectionBehavior(typeof(SalesManager), container);
+
+            hostSalesManager.Open();
+
+            Console.WriteLine("Services started. Press [Enter] to exit.");
+            Console.ReadLine();
+
+            hostSalesManager.Close();
         }
     }
 }
