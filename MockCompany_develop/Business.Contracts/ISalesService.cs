e0f495287c6c8ce8e86b3be635af2b3c3efd5dfd
ddenko <damian.dejnek@gmail.com>
2015-06-10 06:23:50 +0200

    Sales service definition

---
+++ MockCompany_develop/Business.Contracts/ISalesService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.ServiceModel;
+using Business.Entities;
+
+namespace Business.Contracts
+{
+    [ServiceContract]
+    public interface ISalesService
+    {
+        [OperationContract]
+        IEnumerable<Product> GetProducts();
+    }
+}
---
+++ MockCompany_develop/Business.Managers/SalesManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Contracts;
+using Business.Entities;
+
+namespace Business.Managers
+{
+    public class SalesManager:ISalesService
+    {
+        public IEnumerable<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
